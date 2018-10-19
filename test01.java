2640a14cb2a023d55da8e83b525f8f327c84bbf2
Admin <admin@AdmindeMacBook-Pro-40.local>
2018-10-19 17:06:04 +0800

    hello world

---
+++ test01.java
@@ -0,0 +1,12 @@
+/**
+*这是文档
+*/
+public class test01{
+    //这是单行注释
+    public static void main(String[] args){
+        /*
+         *这是多行
+         */
+        System.out.println("123");
+    }
+}
